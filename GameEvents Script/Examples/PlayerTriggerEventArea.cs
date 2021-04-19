cb3eb4617fe9ee251147ca6418d0d1f1bfbac9be
Joao Milone <joaocmil@hotmail.com>
2021-04-19 16:28:14 -0300

    Included scripts
    With a Trigger Player event area as an example

---
+++ GameEvents Script/Examples/PlayerTriggerEventArea.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTriggerEventArea : MonoBehaviour
+{
+    [SerializeField]
+    private GameEvent enterEvent, exitEvent;
+
+    [SerializeField]
+    private string playerTag = "Player";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+            ActivateEnterEvent();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+            ActivateExitEvent();    
+    }
+
+    void ActivateEnterEvent() 
+    {
+        enterEvent?.Invoke();
+        Debug.Log("Player entrou na area!");
+    }
+
+    void ActivateExitEvent()
+    {
+        exitEvent?.Invoke();
+        Debug.Log("Player SAIU da area!");
+    }
+}
